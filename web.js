7295a65c6170b733956a08ab7e771e17d8558f24
GP <rutherford.gh@gmail.com>
2013-07-25 20:03:21 +0000

    Ghana Your Gold Coast

--- web.js
+++ web.js
@@ -2,11 +2,14 @@ var express = require('express');
 var fs = require('fs');
 var index = 'index.html';
 var buf = new Buffer(fs.readFileSync(index));
-var app = express.createServer(express.logger()); 
+var app = express.createServer(); 
 
+
+app.use('/images',express.static(__dirname+'/images'));
 app.get('/', function(request, response) {
   response.send(buf.toString('utf-8'));
 });
+
 
 var port = process.env.PORT || 8080; 
 app.listen(port, function() {
